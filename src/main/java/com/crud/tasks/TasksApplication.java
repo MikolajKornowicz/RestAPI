9a05d5d1674c32dc643318a44943b2657c7b796b
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-04-02 13:58:12 +0200

    heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -13,8 +13,11 @@ public class TasksApplication extends SpringBootServletInitializer {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
+	/*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
 		return application.sources(TasksApplication.class);
 	}
+	
+	 */
 }
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
