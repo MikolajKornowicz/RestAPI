1a6a30b151a22dafaf99b0137dd43e8c7bc4c5ef
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-05-20 15:52:13 +0200

    zadanie29.4

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -4,14 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @EnableScheduling
-@EnableSwagger2
+//@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
 
@@ -19,7 +15,7 @@ public class CoreConfiguration {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
+/*
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -28,4 +24,6 @@ public class CoreConfiguration {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+ */
 }
